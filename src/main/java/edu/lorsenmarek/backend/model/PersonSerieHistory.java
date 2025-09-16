bebf2ef138c5bff4f9d044774d5a0ac58715ce10
Marek Gromko <marek.gromko@hotmail.ca>
2025-09-16 13:36:21 -0400

    model: add PersonSerieHistory

---
+++ src/main/java/edu/lorsenmarek/backend/model/PersonSerieHistory.java
@@ -0,0 +1,19 @@
+package edu.lorsenmarek.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PersonSerieHistory {
+    private Integer personId;
+    private Integer serieId;
+    private Instant dernierVisionage;
+    private Integer dureeEcoute;
+}
