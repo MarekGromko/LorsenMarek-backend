8097a4370035f4770843539ea2f652c4cc38da2d
Marek Gromko <marek.gromko@hotmail.ca>
2025-10-08 10:15:39 -0400

    feat: add needed models for ratings - Episode - Season (needed for episode) - UserEpisodeRating - UserSerieRating

---
+++ src/main/java/edu/lorsenmarek/backend/model/Season.java
@@ -0,0 +1,10 @@
+package edu.lorsenmarek.backend.model;
+
+import java.time.Instant;
+
+public class Season {
+    private Long id;
+    private Long serieId;
+    private String title;
+    private Instant releasedAt;
+}
--- src/main/java/edu/lorsenmarek/backend/model/UserEpisodeRating.java
+++ src/main/java/edu/lorsenmarek/backend/model/UserEpisodeRating.java
@@ -1,2 +1,17 @@
-package edu.lorsenmarek.backend.model;public class UserEpisodeRating {
+package edu.lorsenmarek.backend.model;
+
+import lombok.*;
+
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserEpisodeRating {
+    private Long userId;
+    private Long episodeId;
+    private Instant createdAt;
+    private Instant modifiedAt;
+    private Integer rating;
 }
