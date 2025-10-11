9710559ff11e6ef95cf82b0b68b177628d33af8f
Marek Gromko <marek.gromko@hotmail.ca>
2025-10-11 16:43:50 -0400

    fix: many type in schema.sql - varchar must have a length - some episode tables has serie id in it - fk delete on changed to cascade

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -10,86 +10,89 @@ DROP TABLE if EXISTS `user`;
 
 CREATE TABLE `user`(
     `id` bigint AUTO_INCREMENT,
-    `first_name` varchar,
-    `last_name` varchar,
-    `email` varchar,
-    `title` varchar,
-    `pwd_digest` varchar,
+    `first_name` varchar(255),
+    `last_name` varchar(255),
+    `email` varchar(255),
+    `title` varchar(255),
+    `pwd_digest` varchar(255),
+    `pwd_attempts` int,
+    `pwd_last_attempted_at` timestamp,
     PRIMARY KEY (`id`),
     KEY `email` (`email`)
 );
 
 CREATE TABLE `serie`(
     `id` bigint AUTO_INCREMENT,
-    `title` varchar,
+    `title` varchar(255),
     `released_at` timestamp,
     PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `season` {
+CREATE TABLE `season` (
     `id` bigint AUTO_INCREMENT,
     `serie_id` bigint,
-    `title` varchar,
+    `title` varchar(255),
     `duration` int,
     `released_at` TIMESTAMP,
     PRIMARY KEY (`id`),
     CONSTRAINT `season_fk_serie` FOREIGN KEY (`serie_id`) REFERENCES `Serie` (`id`) ON DELETE CASCADE
-};
+);
 
-CREATE TABLE `episode` {
+CREATE TABLE `episode` (
     `id` bigint AUTO_INCREMENT,
     `season_id` bigint,
-    `title` varchar,
+    `title` varchar(255),
     `duration` int,
     `released_at` timestamp,
     PRIMARY KEY (`id`),
     CONSTRAINT `episode_fk_season` FOREIGN KEY (`season_id`) REFERENCES `Season` (`id`) ON DELETE CASCADE
-};
+);
 
-CREATE TABLE `genre` {
+CREATE TABLE `genre` (
     `id` bigint AUTO_INCREMENT,
-    `label` varchar
-};
+    `label` varchar(255),
+    PRIMARY KEY (`id`)
+);
 
-CREATE TABLE `serie_genre` {
+CREATE TABLE `serie_genre` (
     `serie_id` bigint,
     `genre_id` bigint,
     PRIMARY KEY (`serie_id`, `genre_id`),
     CONSTRAINT `serie_genre_fk_serie` FOREIGN KEY (`serie_id`) REFERENCES `Serie` (`id`) ON DELETE CASCADE,
     CONSTRAINT `serie_genre_fk_genre` FOREIGN KEY (`genre_id`) REFERENCES `Genre` (`id`) ON DELETE CASCADE
-};
+);
 
-CREATE TABLE `user_serie_rating` {
+CREATE TABLE `user_serie_rating` (
     `user_id` bigint,
     `serie_id` bigint,
     `created_at` timestamp,
     `modified_at` timestamp,
     `rating` int,
     PRIMARY KEY (`user_id`, `serie_id`),
-    CONSTRAINT `user_serie_rating_fk_user` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`) ON DELETE SET NULL,
+    CONSTRAINT `user_serie_rating_fk_user` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`) ON DELETE CASCADE,
     CONSTRAINT `user_serie_rating_fk_serie` FOREIGN KEY (`serie_id`) REFERENCES `Serie` (`id`) ON DELETE CASCADE
-};
+);
 
-CREATE TABLE `user_episode_rating` {
+CREATE TABLE `user_episode_rating` (
     `user_id` bigint,
     `episode_id` bigint,
     `created_at` timestamp,
     `modified_at` timestamp,
     `rating` int,
-    PRIMARY KEY (`user_id`, `serie_id`),
-    CONSTRAINT `user_episode_rating_fk_user` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`) ON DELETE SET NULL,
-    CONSTRAINT `user_episode_rating_fk_episode` FOREIGN KEY (`serie_id`) REFERENCES `Episode` (`id`) ON DELETE CASCADE
-};
+    PRIMARY KEY (`user_id`, `episode_id`),
+    CONSTRAINT `user_episode_rating_fk_user` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`) ON DELETE CASCADE,
+    CONSTRAINT `user_episode_rating_fk_episode` FOREIGN KEY (`episode_id`) REFERENCES `Episode` (`id`) ON DELETE CASCADE
+);
 
 CREATE TABLE `user_episode_history` (
     `user_id` bigint NOT NULL,
-    `serie_id` bigint NOT NULL,
-    `last_watch` timestamp NOT NULL DEFAULT current_timestamp(),
-    `instance_watch` int NOT NULL DEFAULT 1,
-    PRIMARY KEY (`user_id`,`serie_id`),
-    KEY `serie_id` (`serie_id`),
+    `episode_id` bigint NOT NULL,
+    `watched_at` timestamp NOT NULL DEFAULT current_timestamp(),
+    `watched_duration` int NOT NULL DEFAULT 1,
+    PRIMARY KEY (`user_id`,`episode_id`),
+    KEY `episode_id` (`episode_id`),
     KEY `user_id` (`user_id`),
-    CONSTRAINT `user_serie_history_fk_user` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`) ON DELETE CASCADE,
-    CONSTRAINT `user_serie_history_fk_serie` FOREIGN KEY (`serie_id`) REFERENCES `Serie` (`id`) ON DELETE CASCADE
+    CONSTRAINT `user_episode_history_fk_user` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`) ON DELETE CASCADE,
+    CONSTRAINT `user_episode_history_fk_episode` FOREIGN KEY (`episode_id`) REFERENCES `Episode` (`id`) ON DELETE CASCADE
 );
 
