37418d707d75d0c11b520da18ef0d1b159e25462
Marek Gromko <marek.gromko@hotmail.ca>
2025-10-13 15:20:45 -0400

    doc: javadoc for DTOs - ErrorResponse - RatingRequest - RatingResponse

--- src/main/java/edu/lorsenmarek/backend/dto/RatingRequest.java
+++ src/main/java/edu/lorsenmarek/backend/dto/RatingRequest.java
@@ -1,4 +1,17 @@
 package edu.lorsenmarek.backend.dto;
 
+/**
+ * Request DTO for rating a media
+ * <p><br>
+ *     <b>Example:</b>
+ *     <pre>{@code
+ *     {
+ *         "rating": 4
+ *     }
+ *     }</pre>
+ * </p>
+ *
+ * @param rating
+ */
 public record RatingRequest(Integer rating) {
 }
