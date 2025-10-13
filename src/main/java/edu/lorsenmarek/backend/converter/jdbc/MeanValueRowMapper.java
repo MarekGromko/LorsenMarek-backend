38f2d2385c6cb8d455b00276dd48068d84a4be80
Marek Gromko <marek.gromko@hotmail.ca>
2025-10-13 15:08:35 -0400

    doc: javadoc For RowMapper MeanValue

--- src/main/java/edu/lorsenmarek/backend/converter/jdbc/MeanValueRowMapper.java
+++ src/main/java/edu/lorsenmarek/backend/converter/jdbc/MeanValueRowMapper.java
@@ -7,17 +7,40 @@ import org.springframework.lang.NonNull;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * Implementation of {@link RowMapper} for {@link MeanValue}
+ *
+ * @author Marek Gromko
+ */
 public class MeanValueRowMapper implements RowMapper<MeanValue> {
     final private Object sumIndex;
     final private Object countIndex;
+
+    /**
+     * Create a row mapper with specified column indexes
+     *
+     * @param sumIndex the index for the column with the sum
+     * @param countIndex the index for the column with the count
+     */
     public MeanValueRowMapper(Integer sumIndex, Integer countIndex) {
         this.sumIndex = sumIndex;
         this.countIndex = countIndex;
     }
-    public MeanValueRowMapper(String sumIndex, String countIndex) {
-        this.sumIndex = sumIndex;
-        this.countIndex = countIndex;
+    /**
+     * Create a row mapper with specified column names
+     *
+     * @param sumName the name for the column with the sum
+     * @param countName the name for the column with the count
+     */
+    public MeanValueRowMapper(String sumName, String countName) {
+        this.sumIndex = sumName;
+        this.countIndex = countName;
     }
+    /**
+     * Create a row mapper with default index
+     * <p>the sum column is by default at index 0</p>
+     * <p>the count column is by default at index 1</p>
+     */
     public MeanValueRowMapper() {
         this.sumIndex = 0;
         this.countIndex = 1;
