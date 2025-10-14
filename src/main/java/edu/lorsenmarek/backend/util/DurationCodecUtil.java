9b6412365f3a98d81d212d75a549618e3f43e762
Marek Gromko <marek.gromko@hotmail.ca>
2025-10-14 15:22:05 -0400

    doc: javadoc for DurationCodecUtil

--- src/main/java/edu/lorsenmarek/backend/util/DurationCodecUtil.java
+++ src/main/java/edu/lorsenmarek/backend/util/DurationCodecUtil.java
@@ -2,7 +2,32 @@ package edu.lorsenmarek.backend.util;
 
 import java.time.Duration;
 
+/**
+ * Utility for decode/encode {@link Duration}
+ *
+ * @see Duration
+ * @author Marek Gromko
+ */
 public class DurationCodecUtil {
+    /**
+     * Decode a string into a {@link Duration}
+     * <p>White spaces & cases are ignored</p>
+     * <p>Any period with a {@code month} or {@code year} may have an undetermined amount of seconds</p>
+     * <p><br/>
+     *     <b>Accepted format</b>
+     *     <pre>{@code
+     *      "T10m" // 10 minutes
+     *      "t 5h 30s" // 5 hours and 50 seconds
+     *      "PT5S" // 5 seconds
+     *      "1d T20h" // 1 day and 20 hours
+     *      "1y 3m 5d" // 1 year, 3 months and 5 day
+     *      "P4DT12H30M" // 4 day 12 hours and 30 minutes
+     *     }</pre>
+     * </p>
+     *
+     * @param encodedDuration the encoded duration
+     * @return the decoded {@link Duration}
+     */
     public static Duration decode(String encodedDuration) {
         if(encodedDuration == null || encodedDuration.isEmpty())
             return Duration.ZERO;
